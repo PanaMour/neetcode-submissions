105fb2e8b12ec4f312c12f1a8e23d7fe1d549662
Panagiotis Mourlas <60044131+PanaMour@users.noreply.github.com>
2026-08-08 17:44:37 +0300

    Add: count-subsequences - submission-0

---
+++ Data Structures & Algorithms/count-subsequences/submission-0.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int NumDistinct(string s, string t) {
+        int[,] dp = new int[s.Length + 1, t.Length + 1];
+        for (int i = 0; i <= s.Length; i++) {
+            dp[i, 0] = 1;
+        }
+        for (int i = 1; i <= s.Length; i++) {
+            for (int j = 1; j <= t.Length; j++) {
+                if (s[i - 1] != t[j - 1]) {
+                    dp[i, j] = dp[i - 1, j];
+                } else {
+                    dp[i, j] = dp[i - 1, j - 1] + dp[i - 1, j];
+                    ;
+                }
+            }
+        }
+        return dp[s.Length, t.Length];
+    }
+}
