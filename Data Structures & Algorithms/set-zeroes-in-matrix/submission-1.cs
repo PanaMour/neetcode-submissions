2d651cdac8b5acbb7750ca9bc136efff9b9f39b6
Panagiotis Mourlas <60044131+PanaMour@users.noreply.github.com>
2026-08-29 17:58:15 +0300

    Add: set-zeroes-in-matrix - submission-1

---
+++ Data Structures & Algorithms/set-zeroes-in-matrix/submission-1.cs
@@ -0,0 +1,40 @@
+public class Solution {
+    public void SetZeroes(int[][] matrix) {
+        bool firstRowHasZero = false;
+        
+        for(int i=0;i<matrix.Length;i++){
+            for(int j=0;j<matrix[0].Length;j++){
+                if(matrix[i][j] == 0)
+                {
+                    if(i == 0) firstRowHasZero = true;
+                    else
+                    {
+                        matrix[i][0] = 0;
+                        matrix[0][j] = 0;
+                    }
+                }
+            }
+        }
+
+        for(int i = 1; i < matrix.Length; i++)
+        {
+            for(int j = 1; j < matrix[0].Length; j++)
+            {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0)
+                {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if (matrix[0][0] == 0)
+        {
+            for(int i = 0; i < matrix.Length; i++) matrix[i][0] = 0;
+        }
+
+        if (firstRowHasZero)
+        {
+            for(int j = 0; j < matrix[0].Length; j++) matrix[0][j] = 0;
+        }
+    }
+}
