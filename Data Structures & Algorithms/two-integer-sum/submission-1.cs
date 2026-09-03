8706a92782f85a258a441ab9a66b2697623d9439
Panagiotis Mourlas <60044131+PanaMour@users.noreply.github.com>
2026-09-03 20:14:13 +0300

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> map = new Dictionary<int, int>();
+        for(int i = 0; i<nums.Length;i++){
+            if(!map.ContainsKey(nums[i])){
+                map[target - nums[i]] = i;
+            }
+            else return new int[] { map[nums[i]], i};
+        }
+        return new int[0];
+    }
+}
